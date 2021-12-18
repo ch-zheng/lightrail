c87c8572f43c5bd859a98c8cbb92a1941a51e27b
Charlie Zheng <chzheng@protonmail.com>
2021-12-17 18:10:30 -0600

    Remove old .cpp files

--- src/scene.cpp
+++
@@ -1,8 +0,0 @@
-#include "scene.hpp"
-#include <rapidjson/document.h>
-using namespace lightrail;
-
-std::vector<Scene> Scene::load_gltf(std::string filename) {
-	std::vector<Scene> result;
-	return result;
-}
--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "mesh.hpp"
-using namespace lightrail;
-
-int main() {
-	Mesh foo = Mesh::load_obj("thing.obj");
-	std::cout << foo << std::endl;
-}
